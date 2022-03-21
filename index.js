a0708de7c0deb73d2875c5407491da850a65f9e7
joel <joelkalema63@gmail.com>
2022-03-21 12:35:55 +0200

    Modified the files

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import Bookstore from './books.js';
+
 const author = document.querySelector('#author');
 const title = document.querySelector('#title');
 const addBookForm = document.querySelector('#add-book');
@@ -26,7 +28,7 @@ function formatAMPM(date) {
 const today = new Date();
 time.textContent = formatAMPM(today);
 
-function SetupTabs() {
+const SetupTabs = () => {
   document.querySelectorAll('.tab__button').forEach((button) => {
     button.addEventListener('click', () => {
       const sideBar = button.parentElement;
@@ -51,50 +53,6 @@ function SetupTabs() {
 document.addEventListener('DOMContentLoaded', () => {
   SetupTabs();
 });
-
-class Bookstore {
-  constructor() {
-    this.books = [];
-  }
-
-  addBook = (author, title) => {
-    const id = this.books.length + 1;
-    const bookData = {
-      id,
-      author,
-      title,
-    };
-    this.books.push(bookData);
-  }
-
-  removeBook = (id) => {
-    this.books.forEach((book) => {
-      if (book.id === id) {
-        this.books.splice(this.books.indexOf(book), 1);
-      }
-    });
-  }
-
-  displayBooks = (list) => {
-    list.innerHTML = '';
-    this.books.forEach((book) => {
-      const li = `<li><div class="list-container"><h3>"${book.title}"</h3>
-                  <p>by ${book.author}</p></div>
-                  <button id="${book.id}" class="remove-btn">Remove</button></li>`;
-      list.innerHTML += li;
-    });
-  }
-
-  setStorage = () => {
-    const formData = JSON.stringify(this.books);
-    localStorage.setItem('books', formData);
-  };
-
-  getStorage = (key) => {
-    this.books = JSON.parse(localStorage.getItem(key));
-    return this.books;
-  };
-}
 
 const bookStore = new Bookstore();
 
