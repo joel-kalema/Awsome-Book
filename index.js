d33550ad15ab03c9099d4fb05d4346cb6331fd8f
joel <joelkalema63@gmail.com>
2022-03-22 12:06:22 +0200

    Modified the content

--- index.js
+++ index.js
@@ -1,54 +1,18 @@
-import Bookstore from './books.js';
+import Bookstore from './modules/books.js';
+import formatAMPM from './modules/time.js';
+import SetupTabs from './modules/switch.js';
 
 const author = document.querySelector('#author');
 const title = document.querySelector('#title');
 const addBookForm = document.querySelector('#add-book');
 const list = document.querySelector('.list');
 const time = document.querySelector('.time');
-
-const formatAMPM = (date) => {
-  let hours = date.getHours();
-
-  let minutes = date.getMinutes();
-
-  const ampm = hours >= 12 ? 'pm' : 'am';
-
-  hours %= 12;
-
-  hours = hours || 12;
-
-  minutes = minutes < 10 ? `0${minutes}` : minutes;
-
-  const time = `${hours}:${minutes} ${ampm}`;
-
-  const match = date.toString().match(/\w{3} \w{3} \d{1,2} \d{4}/);
-
-  return `${match[0]} ${time}`;
-};
+// /*eslint-disable*/
+// let luxon;
+// const { DateTime } = luxon;
+// /*eslint-disable*/
 const today = new Date();
 time.textContent = formatAMPM(today);
-
-const SetupTabs = () => {
-  document.querySelectorAll('.tab__button').forEach((button) => {
-    button.addEventListener('click', () => {
-      const sideBar = button.parentElement;
-      const tabsContainer = sideBar.parentElement;
-      const tabNumber = button.dataset.forTab;
-      const tabToActivate = tabsContainer.querySelector(`.tab__content[data-tab ="${tabNumber}"]`);
-
-      sideBar.querySelectorAll('.tab__button').forEach((button) => {
-        button.classList.remove('tab__button--active');
-      });
-
-      tabsContainer.querySelectorAll('.tab__content').forEach((tab) => {
-        tab.classList.remove('tab__content--active');
-      });
-
-      button.classList.add('tab__button--active');
-      tabToActivate.classList.add('tab__content--active');
-    });
-  });
-};
 
 document.addEventListener('DOMContentLoaded', () => {
   SetupTabs();
